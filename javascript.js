2db12584aca8e6bc78521571effb3fa123f2620b
roberttsouza <48107814+roberttsouza@users.noreply.github.com>
2020-06-16 18:24:07 -0300

    funções adicionada
    adicionado logo personalizado e feito efeito de scroll suave em jquery

--- javascript.js
+++ javascript.js
@@ -11,4 +11,41 @@ for (i = 0; i < acc.length; i++) {
       panel.style.maxHeight = panel.scrollHeight + "px";
     } 
   })
+};
+
+
+/*animação de scroll nos elementos
+
+var $target = $('.anime'),
+    animationClass = 'anime-start';
+
+function animeScroll() {
+  var documentTop = $(document).scrollTop();
+  
+  $target.each(function() {
+    var itemTop = $(this).offset().top;
+    if (documentTop > itemTop-100) {
+      $(this).addClass(animationClass);
+    }else{
+      $(this).removeClass(animationClass);
+    }
+  })
 }
+animeScroll();
+
+*/
+
+/*animação de scroll suave*/
+
+$('nav a').click(function(e){
+  e.preventDefault();
+  var id = $(this).attr('href'),
+  
+      targetOffset = $(id).offset().top,
+      menuHeight = $('nav').innerHeight();
+
+$('html, body').animate({
+  scrollTop: targetOffset - menuHeight - 150
+}, 500);
+
+});
