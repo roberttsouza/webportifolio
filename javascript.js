fd6eb8d6a819f6e5a4baedd1213a0eb1a96af156
roberttsouza <48107814+roberttsouza@users.noreply.github.com>
2020-07-08 20:18:34 -0300

    slideshow
    adicionado o elemento slideshow para uma melhor visualização dos projetos ja existentes.
    faltando ainda colocar um hover por cima da imagem para ser redirecionado pro repositório git.

--- javascript.js
+++ javascript.js
@@ -54,29 +54,28 @@ $('html, body').animate({
 var slideIndex = 1;
 showSlides(slideIndex);
 
-function plusSlide(n){
+// Next/previous controls
+function plusSlides(n) {
   showSlides(slideIndex += n);
 }
-function currentSlide(n){
+
+// Thumbnail image controls
+function currentSlide(n) {
   showSlides(slideIndex = n);
 }
 
-function showSlides(n){
+function showSlides(n) {
   var i;
-  var slide =  document.getElementsByClassName("mySlide");
-  var dot = document.getElementsByName("dot");
-
-  if(n > slide.length)
-  {slideIndex = 1}
-  if(n < 1)
-  {slideIndex = slide.length}
-  for (i = 0; i < slide.length; i++){
-    slide[i].style.display = "none"
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++){
-    dors[i].className = dors[i].className.replace
-    ("ative", "");
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display= "block";
-  dots[slideIndex-1].className += "active";
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 }
